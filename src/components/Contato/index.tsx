e7441185c5d5b93adb2cd2fdc2074f2cfdaee04c
Wanderson Gabriel <gabriel_wanderson@hotmail.com>
2024-11-09 12:25:06 -0300

    Criação de Estado dos botões para salvar, editar, cancelar e remover.

--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -1,30 +1,56 @@
+import { useState } from 'react'
 import * as S from './styles'
 import { AiOutlineMail, AiOutlinePhone } from 'react-icons/ai'
 import { BsPencilSquare, BsTrash } from 'react-icons/bs'
 
-const Contato = () => (
-  <S.Card>
-    <S.Titulo>
-      <div>Wanderson Gabriel da Silva</div>
-    </S.Titulo>
-    <S.Paragrafo>
-      <AiOutlinePhone />
-      (99) 99999-9999
-    </S.Paragrafo>
-    <S.Paragrafo>
-      <AiOutlineMail /> teste.email@hotmail.com
-    </S.Paragrafo>
-    <ul>
-      <S.Botao type="button">
-        <BsPencilSquare />
-        Editar
-      </S.Botao>
-      <S.Botao type="button">
-        <BsTrash />
-        Excluir
-      </S.Botao>
-    </ul>
-  </S.Card>
-)
+type Props = {
+  nome: string
+  email: string
+  telefone: number
+}
+
+const Contato = ({ nome, email, telefone }: Props) => {
+  const [estaEditando, setEstaEditando] = useState(false)
+
+  return (
+    <S.Card>
+      <S.Titulo>
+        <div>{nome}</div>
+      </S.Titulo>
+      <S.Paragrafo>
+        <AiOutlinePhone />
+        {telefone}
+      </S.Paragrafo>
+      <S.Paragrafo>
+        <AiOutlineMail /> {email}
+      </S.Paragrafo>
+      <ul>
+        {estaEditando ? (
+          <>
+            <S.Botao>
+              <BsPencilSquare />
+              Salvar
+            </S.Botao>
+            <S.Botao type="button" onClick={() => setEstaEditando(false)}>
+              <BsTrash />
+              Cancelar
+            </S.Botao>
+          </>
+        ) : (
+          <>
+            <S.Botao type="button" onClick={() => setEstaEditando(true)}>
+              <BsPencilSquare />
+              Editar
+            </S.Botao>
+            <S.Botao type="button">
+              <BsTrash />
+              Excluir
+            </S.Botao>
+          </>
+        )}
+      </ul>
+    </S.Card>
+  )
+}
 
 export default Contato
--- src/containers/ListaDeContatos/index.tsx
+++ src/containers/ListaDeContatos/index.tsx
@@ -2,17 +2,38 @@ import Contato from '../../components/Contato'
 import { MainContainer } from '../../styles'
 import { CardContato } from './style'
 
+const contatos = [
+  {
+    nome: 'Bruno Lacerda',
+    telefone: 99999999999,
+    email: 'teste@mail.com'
+  },
+  {
+    nome: 'Lucas Mito',
+    telefone: 88888888888,
+    email: 'lucas@mail.com'
+  },
+  {
+    nome: 'Glória Maria',
+    telefone: 77777777777,
+    email: 'gloramaria@mail.com'
+  }
+]
+
 const ListaDeContatos = () => (
   <MainContainer>
     <ul>
-      <li>
-        <CardContato>
-          <Contato />
-          <Contato />
-          <Contato />
-          <Contato />
-        </CardContato>
-      </li>
+      <CardContato>
+        {contatos.map((agenda) => (
+          <li key={agenda.nome}>
+            <Contato
+              nome={agenda.nome}
+              email={agenda.email}
+              telefone={agenda.telefone}
+            />
+          </li>
+        ))}
+      </CardContato>
     </ul>
   </MainContainer>
 )
